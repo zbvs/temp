685e26ea51efd5197c5cc9b713f8486fe631d1a4
zbvs <zbvs12@gmail.com>
2019-04-20 18:44:00 +0900

    Update temp.inc.php

--- temp.inc.php
+++ temp.inc.php
@@ -1,6 +1,11 @@
 <?php
 $fp = fopen('./index.php','r') or die('open fail');
 $size = filesize('./index.php');
+echo $size,'
+';
+$size = filesize("/web-serveur/ch45/index.php");
+echo $size,'
+';
 $data = fread($fp, $size);
 $data = base64_encode( $data );
 echo $data.'
