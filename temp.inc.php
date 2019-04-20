6cbdece64e4b914bab48a522f705fc5cb3df7cae
zbvs <zbvs12@gmail.com>
2019-04-20 19:04:07 +0900

    Update temp.inc.php

--- temp.inc.php
+++ temp.inc.php
@@ -7,8 +7,37 @@ echo 'size::'.$size,'
 
 $data = fread($fp, $size);
 $data = base64_encode( $data );
-echo 'data'.$data.'
-=
+echo 'data:'.$data.'
 ';
+
+$fp = fopen('./home.inc.php','r') or die('open fail');
+$size = filesize('./home.inc.php');
+echo 'size::'.$size,'
+';
+
+$data = fread($fp, $size);
+$data = base64_encode( $data );
+echo 'data:'.$data.'
+';
+$fp = fopen('./cv.inc.php','r') or die('open fail');
+$size = filesize('./cv.inc.php');
+echo 'size::'.$size,'
+';
+
+$data = fread($fp, $size);
+$data = base64_encode( $data );
+echo 'data:'.$data.'
+';
+
+$fp = fopen('./contact.inc.php','r') or die('open fail');
+$size = filesize('./contact.inc.php');
+echo 'size::'.$size,'
+';
+
+$data = fread($fp, $size);
+$data = base64_encode( $data );
+echo 'data:'.$data.'
+';
+
 exit(1);
 ?>
